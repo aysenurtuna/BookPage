561197106bd895d397c3da6597c93d146255caee
TOSHIBA <TOSHIBA@DESKTOP-0544ADA>
2023-09-19 15:43:10 +0300

    Add project files.

---
+++ MVCproject/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using MVCproject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCproject.Controllers
+{
+    public class HomeController : Controller
+    {
+        BookPageEntities1 db = new BookPageEntities1();
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Books> books = db.Books.ToList();
+            ViewBag.Types = db.Books.Select(i => i.Type).Distinct().ToList();
+            return View(books);
+        }
+        public ActionResult Kategoriler(string type)
+        {
+            var books = db.Books.Where(i => i.Type.ToLower() == type.ToLower()).ToList();
+            ViewBag.Liste = books;
+            ViewBag.Types = db.Books.Select(i => i.Type).Distinct().ToList();
+            return View();
+        }
+        public ActionResult Details(int id)
+        {
+            var book = db.Books.Where(i => i.ID == id).ToList();
+            bool exist = db.List.Any(i => i.ID == id);
+            if (exist)
+            {
+                bool reading = db.List.Where(i => i.ID == id).Select(i => i.IsRead).FirstOrDefault();
+                ViewBag.Reading = reading;
+                ViewBag.Exist = exist;
+            }
+            else
+            {
+                ViewBag.Exist = false;
+            }
+            ViewBag.Book = book;
+            return View();
+        }
+        public ActionResult Search(string query)
+        {
+            var books = db.Books.Where(i => i.BookName.Contains(query)).ToList();
+            ViewBag.Search = books;
+            return View();
+        } 
+    }
+}
---
+++ MVCproject/Controllers/ListeController.cs
@@ -0,0 +1,132 @@
+﻿using MVCproject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCproject.Controllers
+{
+    public class ListeController : Controller
+    {
+        BookPageEntities1 db = new BookPageEntities1();
+        // GET: Litse
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Okuduklarim()
+        {
+            var books = db.List.Where(i => i.IsRead == true).ToList();
+            if (books.Count == 0)
+            {
+                ViewBag.Text = "Listenizde hiç kitap yok!";
+                return View("Alert");
+            }
+            else
+            {
+                ViewBag.Okuduklarim = books;
+                return View();
+            } 
+        }
+        public ActionResult Okunacaklar()
+        {
+            var books = db.List.Where(i => i.IsRead == false).ToList();
+            if (books.Count == 0)
+            {
+                ViewBag.Text = "Listenizde hiç kitap yok!";
+                return View("Alert");
+            }
+            else
+            {
+                ViewBag.Okunacaklar = books;
+                return View();
+            }
+        }
+        public ActionResult Add(int id, bool reading)
+        {
+            var book = db.Books.Where(i => i.ID == id).FirstOrDefault();
+            var control = db.List.Any(i => i.ID == id);
+            if (control)
+            {
+                ViewBag.Text = "Kitap zaten listenizde ekli!";
+                return View("Alert");
+            }
+            else
+            {
+                List list = new List();
+                list.BookName = book.BookName;
+                list.Author = book.Author;
+                list.Page = (int)book.Page;
+                list.Type = book.Type;
+                list.IsRead = reading;
+                db.List.Add(list);
+                db.SaveChanges();
+                return View();
+            }
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+
+            var list = db.List.Where(i => i.ID == id).FirstOrDefault();
+            db.List.Remove(list);
+            var sonuc = db.SaveChanges();
+
+            if (sonuc >= 0)
+            {
+
+                return Json(new { hasError = false, Result = true }, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(new { hasError = true, Result = false }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        [HttpPost]
+        public ActionResult Read(int id)
+        {
+            var list = db.List.Where(i => i.ID == id).FirstOrDefault();
+            list.IsRead = true;
+            var sonuc = db.SaveChanges();
+
+            if (sonuc >= 0)
+            {
+
+                return Json(new { hasError = false, Result = true }, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(new { hasError = true, Result = false }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        public ActionResult ReadFalse()
+        {
+            var books = db.List.Where(x => x.IsRead == false).ToList();
+            if(books.Count == 0)
+            {
+                ViewBag.Text = "Listenizde hiç kitap yok!";
+                return View("Alert");
+            }
+            else
+            {
+                 ViewBag.Okunacaklar = books;
+                 return View();
+            }
+        }
+        public ActionResult ReadTrue()
+        {
+            var books = db.List.Where(x => x.IsRead == true).ToList();
+            if (books.Count == 0)
+            {
+                ViewBag.Text = "Listenizde hiç kitap yok!";
+                return View("Alert");
+            }
+            else
+            {
+                ViewBag.Okuduklarim = books;
+                return View();
+            }
+        }
+    }
+}
